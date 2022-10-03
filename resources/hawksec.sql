ada2563ddb71b8616f2ecbcd314bcf5275b76961
lijinmiao <beikejinmiao@gmail.com>
2022-10-04 00:07:29 +0800

    更新数据库表结构,添加建库SQL语句

---
+++ resources/hawksec.sql
@@ -0,0 +1,50 @@
+CREATE TABLE crawlstat (
+    id          INTEGER PRIMARY KEY AUTOINCREMENT,
+    origin     	TEXT NOT NULL,
+    resp_code 	INT,
+    [desc]      TEXT,
+    create_time DATETIME DEFAULT (datetime('now', 'localtime'))
+);
+
+
+
+CREATE TABLE extractor (
+    id             INTEGER  PRIMARY KEY AUTOINCREMENT,
+    origin         TEXT     NOT NULL,
+    sensitive_type INT      NOT NULL,
+    sensitive_name VARCHAR (128) NOT NULL,
+    content        TEXT     NOT NULL,
+    count          INT      DEFAULT 1,
+    [desc]         TEXT,
+    create_time    DATETIME DEFAULT (datetime('now', 'localtime') )
+);
+
+
+CREATE TABLE sensitives (
+    id             INTEGER       PRIMARY KEY AUTOINCREMENT,
+    content        TEXT          NOT NULL,
+    content_name   TEXT,
+    sensitive_type INT           NOT NULL,
+    sensitive_name VARCHAR (128) NOT NULL,
+    origin         TEXT          NOT NULL,
+    [desc]         TEXT,
+    create_time    DATETIME      DEFAULT (datetime('now', 'localtime') )
+);
+
+
+CREATE TABLE whitelist (
+    id          INTEGER PRIMARY KEY AUTOINCREMENT,
+    white_type 	INT NOT NULL,
+    ioc     	TEXT NOT NULL,
+    [desc]      TEXT,
+    create_time DATETIME DEFAULT (datetime('now', 'localtime'))
+);
+
+
+CREATE TABLE filetype (
+    id          INTEGER      PRIMARY KEY AUTOINCREMENT,
+    category    VARCHAR (32) NOT NULL,
+    suffix      VARCHAR (32) NOT NULL,
+    mime_type   TEXT,
+    create_time DATETIME     DEFAULT (datetime('now', 'localtime') )
+);
