6caf7385b1bbe5ec8d7782cc50501b2266f41415
lijinmiao <beikejinmiao@gmail.com>
2023-05-30 21:41:41 +0800

    hawksec.yaml文件偶然损坏时(原因未知,疑似频繁写入导致),启动备份配置

--- libs/pyaml.py
+++ libs/pyaml.py
@@ -3,6 +3,32 @@
 import yaml
 from libs.singleton import Singleton
 from conf.paths import CONF_PATH
+from libs.logger import logger
+
+_yaml_backup = {
+    "timeout": 5,
+    "ssh": {
+        "password": None,
+        "host": None,
+        "port": 22,
+        "username": "root",
+        "path": "/tmp"
+    },
+    "target": "",
+    "builtin_alexa": True,
+    "protocol": "https",
+    "charset": "auto",
+    "metric": {
+        "crawl_total": 0,
+        "expend_time": "",
+        "crawl_failed": 0,
+        "origin_hit": 0,
+        "start_time": "",
+        "idcard_count": 0,
+        "keyword_find": 0,
+        "exturl_count": 0
+    }
+}
 
 
 class Config(object):
@@ -13,6 +39,11 @@ class Config(object):
         self.cfg = dict()
         with open(filename, encoding='utf-8') as f:
             self.cfg = yaml.load(f, Loader=yaml.FullLoader)
+        try:
+            self.cfg['metric']['crawl_total']
+        except:
+            logger.warning('配置文件%s疑似损坏,恢复默认配置' % CONF_PATH)
+            self.cfg = _yaml_backup
 
     def __setitem__(self, key, value):
         self.cfg[key] = value
